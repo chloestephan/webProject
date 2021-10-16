b01abb3d400dd796b422131ae7a93045d8a0a552
BIgra77 <44897812+BIgra77@users.noreply.github.com>
2021-10-16 12:09:18 +0200

    update id

--- server/data/articles.js
+++ server/data/articles.js
@@ -3,76 +3,76 @@ const articles = [
     id: 1,
     class:'filterDiv trousers',
     name: 'Trackpants',
-    description: 'Ajouter description',
+    description: 'a pair of trousers that I recommend. In any case I have the same and I am known to have good taste :)',
     image: 'https://tse2.mm.bing.net/th?id=OIP.lc4WVadYWNzFNp9a7kzA6gHaHa&pid=Api',
     price: 25,
     stack: [ 'Trousers' ]
   },
   {
-    id: 1,
+    id: 2,
     class:'filterDiv trousers',
     name: 'Shorts',
-    description: 'Ajouter description',
+    description: 'To be used in summer of course. You can also use it in winter but your calves will be cold.',
     image: 'https://tse2.mm.bing.net/th?id=OIP.EM-HHfBb9o3-b6u30dSecQHaJQ&pid=Api',
     price: 20,
     stack: [ 'Trousers' ]
   },
   {
-    id: 1,
+    id: 3,
     class:'filterDiv trousers',
     name: 'Pants',
-    description: 'Ajouter description',
+    description: 'Classy trousers for special occasions. For your aunt\'s wedding for example',
     image: 'https://tse1.mm.bing.net/th?id=OIP.fTJ0U1BVfPSdeGYQATc-QgHaHa&pid=Api',
     price: 60,
     stack: [ 'Trousers' ]
   },
   {
-    id: 2,
+    id: 4,
     class:'filterDiv filterDiv tops',
     name: 'T-Shirt',
-    description: 'Ajouter description',
+    description: 'A very ordinary tee shirt. But made by us, it becomes incredible',
     image: 'https://tse2.mm.bing.net/th?id=OIP.h4aKIqwBDttExVRcqW8FTgHaIw&pid=Api',
     price: 15,
     stack: [ 'Tops' ]
   },
   {
-    id: 2,
+    id: 5,
     class:'filterDiv tops',
     name: 'Dress shirt',
-    description: 'Ajouter description',
+    description: 'If you go to your job interview wearing this shirt, you will get the job. I promise ! ',
     image: 'https://tse2.mm.bing.net/th?id=OIP.p8duL3VMclVBcq9eSwYlyAHaHa&pid=Api',
     price: 30,
     stack: [ 'Tops' ]
   },
   {
-    id: 3,
+    id: 6,
     class:'filterDiv shoes',
     name: 'Baskets',
-    description: 'Ajouter description',
+    description: 'Dont pretend you are going to play sports with it. Very nice though',
     image: 'https://tse2.mm.bing.net/th?id=OIP.ODm2Gvym6ndd1SotdqxbWAHaGl&pid=Api',
     price: 120,
     stack: [ 'Shoes' ]
   },
   {
-    id: 3,
+    id: 7,
     class:'filterDiv shoes',
     name: 'Boots',
-    description: 'Ajouter description',
+    description: 'They are veryyyyyy expensive but they are veryyyyyyy good. Like really',
     image: 'https://tse4.mm.bing.net/th?id=OIP.K2cZAvRwLhrxQd8Z-nBaUwHaHa&pid=Api',
     price: 400,
     stack: [ 'Shoes' ]
   },
   {
-    id: 4,
+    id: 8,
     class:'filterDiv accessories',
     name: 'Necklace',
-    description: 'Ajouter description',
+    description: 'A nice necklace. Out of inspiration',
     image: 'https://tse2.mm.bing.net/th?id=OIP.P0D58aAnVY3nSr5AOyATOgHaKd&pid=Api',
     price: 1500,
     stack: [ 'Accessories' ]
   },
   {
-    id: 4,
+    id: 9,
     class:'filterDiv accessories',
     name: 'Bracelet',
     description: 'Ajouter description',
@@ -81,7 +81,7 @@ const articles = [
     stack: [ 'Accessories' ]
   },
   {
-    id: 4,
+    id: 10,
     class:'filterDiv accessories',
     name: 'Sweater',
     description: "Ajouter description",
